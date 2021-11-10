25b85c21790e5659927a08a2107b8405e0a5beb0
JRGCast <joao.ralph1@gmail.com>
2021-11-10 10:51:25 -0300

    instaladas dependencias, iniciado model e esqueleto do servidor

---
+++ index.js
@@ -0,0 +1,28 @@
+const cors = require('cors');
+const express = require('express');
+
+const PORT = process.env.PORT;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (_req, res) => {
+  res.send(`Porta ${PORT} Ok!`);
+});
+
+// app.use('/company', companyRouter);
+// app.use('/clients', clientsRouter);
+
+app.use((error, _req, res, _next) => {
+  console.log('Final middleware error');
+  if (error.status && error.message) {
+    res.status(error.status).json({ message: { error_type: error.type, error_message: error.message } });
+  }
+  res.status(500).json({ message: error.message || 'Erro desconhecido ao servidor' });
+});
+
+
+app.listen(PORT, () => console.log(`Escutando porta ${PORT}`));
